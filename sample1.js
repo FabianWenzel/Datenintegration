62df74c6612a5fddfb63693fdadc5248f41ec250
Fabian Wenzel <fw031@hdm-stuttgart.de>
2016-04-10 20:17:47 +0200

    ppn ausgabe

--- sample1.js
+++ sample1.js
@@ -16,12 +16,23 @@ var stream = fs.createReadStream("Liste_PPN-ExNr_HSHN-libre.csv");
 // .fromStream This accepted a readable stream to parse data from.
 // Setting the headers option will cause change each row to an object rather than an array.
 csv
-//.fromStream(stream, {headers : true})
-    .fromStream(stream, {headers : ["PPN", "ExNr", "Signatur", "Barcode", "Sigel"]})
+    .fromStream(stream, {headers : true})
+    //.fromStream(stream, {headers : ["PPN", "ExNr", "Signatur", "Barcode", "Sigel"]})
     // same as headers : true
+    .transform(function(data) {
+        // Transform the Data in the Stream, length of PPN should be 9 Copyed from Julius Renner.
+        transformedData = {
+            ppn: '0'.repeat(9 - data.PPN.length) + data.PPN,
+            exemplarDatensatznr: data['Exemplar-Datensatznr'],
+            signatur: data.Signatur,
+            barcode: data.Barcode,
+            sigel: data.Sigel,
+        };
+        return transformedData;
+    })
 
-    .on("data", function(data){
-        console.log(data);
+    .on("data", function(row){
+        console.log(row.ppn);
     })
     .on("end", function(){
         console.log("done");
