366ce08bd14a600a891d3f3f4a2eead5d3e2d206
Fabian Wenzel <fw031@hdm-stuttgart.de>
2016-04-17 21:15:00 +0200

    Load all ppn's , go to swb.bsz-bw.de.."+ppn", create model Item for mongo db and save it to mongo db

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
 var http = require('http');
 var request = require("request");
+var fs = require('fs');
 //var xml2json = require('xml2json');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/swb');
 
 var Item = mongoose.model('Item', {
-    ppn: Number,
+    ppn: String,
     xml: String
 });
 
-var item = new Item({
-    ppn: 469971,
-    xml: '<xml></xml>'
+var swbFetcher = function(ppn, callback) {
+    var url = 'http://swb.bsz-bw.de/sru/DB=2.1/username=/password=/?query=pica.ppn+%3D+%' + ppn +'%22&version=1.1&operation=searchRetrieve&stylesheet=http%3A%2F%2Fswb.bsz-bw.de%2Fsru%2F%3Fxsl%3DsearchRetrieveResponse&recordSchema=marc21&maximumRecords=1&startRecord=1&recordPacking=xml&sortKeys=none&x-info-5-mg-requestGroupings=none';
+
+    request(url, function(error, response, data) {
+        var item = new Item({
+            ppn: ppn,
+            xml: data
+        });
+
+        if (typeof callback === "function") {
+            callback(item);
+        }
+    });
+};
+
+var saveItem = function(item) {
+    item.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('saved');
+        }
+    });
+};
+
+var readJsonFile = function(path, callback) {
+    fs.readFile(path, 'utf8', function(err,data) {
+        if (err) throw err;
+
+        var obj = JSON.parse(data);
+
+        if (typeof callback === "function") {
+            callback(obj);
+        }
+    });
+};
+
+
+readJsonFile('result.txt', function(data) {
+    data.forEach(function(item) {
+        swbFetcher(item.ppn, function(item) {
+            saveItem(item);
+        });
+    })
 });
-
-item.save(function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('saved');
-    }
-});
-
-
-// var url = 'http://swb.bsz-bw.de/sru/DB=2.1/username=/password=/?query=pica.ppn+%3D+%22435169297%22&version=1.1&operation=searchRetrieve&stylesheet=http%3A%2F%2Fswb.bsz-bw.de%2Fsru%2F%3Fxsl%3DsearchRetrieveResponse&recordSchema=marc21&maximumRecords=1&startRecord=1&recordPacking=xml&sortKeys=none&x-info-5-mg-requestGroupings=none';
-//
-// request(url, function(error, response, data) {
-//     console.log(data);
-//     var json = xml2json.toJson(data);
-//     console.log(json);
-// });
